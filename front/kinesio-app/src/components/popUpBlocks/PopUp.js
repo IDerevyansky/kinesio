9937749b65a6c7324d2735c7d6013cdd01a621c5
“IDerevyansky” <IDerevyansky@yandex.ru>
2020-09-03 02:47:22 +0300

    -

--- front/kinesio-app/src/components/popUpBlocks/PopUp.js
+++ front/kinesio-app/src/components/popUpBlocks/PopUp.js
@@ -6,6 +6,7 @@ document.body.style.overflowY="hidden";
 
 
 function PopUp() {
+
   return (
 
     //Background for test view mock up site
@@ -14,9 +15,36 @@ function PopUp() {
       <div className="bgPopUp">
         <div className="popUpContainer">
           <div className="popUpBlock">
-            <div className="popUpContent">
-              <p>Записаться</p>
-            </div>
+            <div className="crossClosed"></div>
+              <div className="popUpContent">
+                <p className="textCenter m24-b f-22 f-w-600">Записаться</p>
+                <div className="containerButton">
+
+                    <div className="left inlineBlock">
+
+                        <p className="textCenter">Записаться в один клик</p>
+                          <div className="messageBox">
+                            <div className="whatsapp inlineBlock"></div>
+                            <div className="telegram inlineBlock m24-l"></div>
+                            <div className="viber inlineBlock m24-l"></div>
+                          </div>
+
+                    </div>
+
+                    <div className="right inlineBlock" >
+
+                      <div style={{marginLeft:"46px"}}>
+                        <p className="textCenter">Запись через Email</p>
+                        <a href="mailto:iderevyansky@yandex.ru" rel="noopener noreferrer" target="_blank"><div className="singUp"></div></a>
+                      </div>
+
+                    </div>
+
+
+                </div>
+                <p className="textCenter m24-t f-16">или</p>
+                <a  href="tel:8-977-414-74-77" rel="noopener noreferrer" target="_blank" ><p className="textCenter m24-t f-22 f-w-600">8 977 414 74 77</p></a>
+              </div>
           </div>
         </div>
       </div>
