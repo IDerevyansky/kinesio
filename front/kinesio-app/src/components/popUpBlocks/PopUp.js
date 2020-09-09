e41ea5817d0a439ef7e2e271e2c573be655a7644
IDerevyansky <IDerevyansky@yandex.ru>
2020-09-10 01:18:43 +0300

    Make was corectly posts mail latter
    make will corectly post massagers messeg

--- front/kinesio-app/src/components/popUpBlocks/PopUp.js
+++ front/kinesio-app/src/components/popUpBlocks/PopUp.js
@@ -16,7 +16,7 @@ class PopUp extends Component {
 
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop; //Узнаем величину прокрутки страницы
     var heightWindow = window.innerHeight / 100 * 20; //Получаем 20% от высоты видемой области страницы
-
+    // var widthWindow = window.innerWidth // На основе этого параметра сделать функцию выбора ссылки  для viber descktop или mobile
 
     return (
 
@@ -35,9 +35,9 @@ class PopUp extends Component {
 
                                 <p className="textCenter">Записаться в один клик</p>
                                   <div className="messageBox">
-                                    <div className="whatsapp"></div>
-                                    <div className="telegram m24-l"></div>
-                                    <div className="viber m24-l"></div>
+                                    <a href="https://wa.me/+79653121776?text=urlencodedtext" rel="noopener noreferrer" target="_blank"><div className="whatsapp"></div></a>
+                                    <a href="tg://resolve?domain=@IDerevyansky" rel="noopener noreferrer" target="_blank"><div className="telegram m24-l"></div></a>
+                                    <a href="viber://chat?nnumber=+79653121776?text=qqqqqqq" rel="noopener noreferrer" target="_blank"><div className="viber m24-l"></div></a>
                                   </div>
 
                             </div>
@@ -46,7 +46,7 @@ class PopUp extends Component {
 
                               <div className="margin">
                                 <p className="textCenter">Запись через Email</p>
-                                <a href="mailto:iderevyansky@yandex.ru" rel="noopener noreferrer" target="_blank"><div className="singUp"></div></a>
+                                <a href="mailto:iderevyansky@yandex.ru?subject=Мое письмо&body=Мой текст в этом письме" rel="noopener noreferrer" target="_blank"><div className="singUp"></div></a>
                               </div>
 
                             </div>
