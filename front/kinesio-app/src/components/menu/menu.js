dd3c37cf91b1ad2cd0d2874ac71fd9254c328dd4
IDerevyansky <IDerevyansky@yandex.ru>
2020-09-17 00:23:41 +0300

    -

--- front/kinesio-app/src/components/menu/menu.js
+++ front/kinesio-app/src/components/menu/menu.js
@@ -11,7 +11,50 @@ constructor(props){
 
 
 
+
+
+componentDidMount(){
+
+  var resize = () =>{
+
+    var menuContent, logoBox, itemBox, sumTwoBox, ul;
+
+    itemBox =     document.getElementsByClassName('itemBox');
+    logoBox =     document.getElementsByClassName('logoBox');
+    menuContent = document.getElementsByClassName('menuContent');
+    ul =          document.getElementsByClassName('ul');
+    sumTwoBox =   itemBox[0].clientWidth + logoBox[0].clientWidth;
+
+    // if(menuContent[0].clientWidth <= sumTwoBox){
+    //   console.log('меньше');
+    // }
+    //
+    // var li = () =>{
+    //
+    //   for (var i = 0; i < ul[0].childElementCount; i++) {
+    //     <li>Item</li>
+    //   }
+    //
+    // }
+
+
+
+
+
+
+
+    // console.log('menuContent: '+menuContent[0].clientWidth+' logoBox: '+logoBox[0].clientWidth+' itemBox: '+itemBox[0].clientWidth);
+
+  }
+
+  window.addEventListener('resize', ()=>{resize()});
+
+}
+
+
 render(){
+
+
 
   return(
     <>
@@ -25,12 +68,8 @@ render(){
 
               <div className="itemBox">
 
-                  <ul className="f-w-600">
-                    <li>Item</li>
-                    <li>Item</li>
-                    <li>Item</li>
-                    <li>Item</li>
-                    <li>Item</li>
+                  <ul className="f-w-600 ul">
+
                   </ul>
 
                   <div className="mobileMenu"></div>
