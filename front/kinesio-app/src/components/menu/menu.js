0f34993c34fd277d498d82d3491790a307d8558a
“IDerevyansky” <IDerevyansky@yandex.ru>
2020-09-28 19:02:31 +0300

    -

--- front/kinesio-app/src/components/menu/menu.js
+++ front/kinesio-app/src/components/menu/menu.js
@@ -6,7 +6,8 @@ class Menu extends Component {
 constructor(props){
   super(props);
   this.state = {
-    listItemMain:[]
+    listItemMain:[],
+    listItemMobile:[]
   }
 
 }
@@ -17,84 +18,72 @@ constructor(props){
 
 componentDidMount(){
 
-
-
-  var menuContent, logoBox, itemBox, sumTwoBox, listItem, listItemMain = [], listItemMobile = [];
-
-  itemBox =     document.getElementsByClassName('itemBox');
-  logoBox =     document.getElementsByClassName('logoBox');
-  menuContent = document.getElementsByClassName('menuContent');
-  sumTwoBox =   itemBox[0].clientWidth + logoBox[0].clientWidth;
-  listItem = [
-    {
-      name:'Item1',
-      url:'#'
-    },
-    {
-      name:'Item2',
-      url:'#'
-    },
-    {
-      name:'Item3',
-      url:'#'
-    },
-    {
-      name:'Item4',
-      url:'#'
-    },
-    {
-      name:'Item5',
-      url:'#'
-    }
-  ];
-
-// this.setState({ listItemMain:listItem });
-
   var resize = () =>{
 
 
-    for (var i = 0; i < listItem.length; i++) {
+    var menuContent, logoBox, itemBox, sumTwoBox, listItem, listItemMain = [], listItemMobile = [];
 
+    itemBox =     document.getElementsByClassName('itemBox');
+    logoBox =     document.getElementsByClassName('logoBox');
+    menuContent = document.getElementsByClassName('menuContent');
+    sumTwoBox =   itemBox[0].clientWidth + logoBox[0].clientWidth;
+    listItem = [
+      {
+        name:'Item1',
+        url:'#'
+      },
+      {
+        name:'Item2',
+        url:'#'
+      },
+      {
+        name:'Item3',
+        url:'#'
+      },
+      {
+        name:'Item4',
+        url:'#'
+      },
+      {
+        name:'Item5',
+        url:'#'
+      }
+    ];
+
+    // this.setState({ listItemMain:listItem });
+    // this.setState({ listItemMobile:listItem });
+
+
+
+    console.log('Контент');
+    console.log(menuContent[0].clientWidth);
+    console.log('Сумма двух блоков');
+    console.log(sumTwoBox);
+
+    for (let i = 0; i < listItem.length; i++) {
 
       if(menuContent[0].clientWidth > sumTwoBox){
 
-        listItemMain[i] = listItem[i];
-        listItemMobile.splice() ;
-        console.log('-----------------');
-        console.log('Основное меню');
-        console.log(listItemMain);
-        console.log('Мобильное меню');
-        console.log(listItemMobile);
-        console.log('-----------------');
-
-        this.setState({ listItemMain:listItemMain });
+      console.log('true');
         // listItemMain[i] = listItem[i];
-        // // listItemMobile[i] = listItemMain.shift()
-        // // console.log(listItemMain);
         // this.setState({ listItemMain:listItemMain });
 
       }else{
 
-        // listItemMobile = listItem[i];
-        // listItemMain.pop();
-        // console.log('Мобиьное меню');
-        // console.log(listItemMobile);
+        console.log('false');
+        // listItemMobile[i] = listItem[i];
+        // this.setState({ listItemMobile:listItemMain });
 
-          // listItemMain = listItemMain.pop();
-          // console.log(listItemMain);
-          // this.setState({ listItemMain:listItemMain });
+            }
 
-           }
+          }
 
+// console.log(listItemMain);
+// console.log('----------');
+// console.log(listItemMobile);
+// console.log('----------');
 
-
-    }
-
-
-
-
-
-
+// this.setState({ listItemMain:listItemMain, listItemMobile:listItemMobile });
 
                   }
 
