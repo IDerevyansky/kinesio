249acb55f9a927b69e60933552d8db98c9017afe
“IDerevyansky” <IDerevyansky@yandex.ru>
2020-09-29 18:26:40 +0300

    -

--- front/kinesio-app/src/components/menu/menu.js
+++ front/kinesio-app/src/components/menu/menu.js
@@ -6,8 +6,7 @@ class Menu extends Component {
 constructor(props){
   super(props);
   this.state = {
-    listItemMain:[],
-    listItemMobile:[]
+    listItem:'пусто'
   }
 
 }
@@ -18,96 +17,53 @@ constructor(props){
 
 componentDidMount(){
 
-  var resize = () =>{
+  var menuContent, logoBox, itemBox, sumTwoBox, listItem;
+
+  itemBox =     document.getElementsByClassName('itemBox');
+  logoBox =     document.getElementsByClassName('logoBox');
+  menuContent = document.getElementsByClassName('menuContent');
+  sumTwoBox =   itemBox[0].clientWidth + logoBox[0].clientWidth;
+
+  listItem = [
+    {
+      name:'Item1',
+      url:'#'
+    },
+    {
+      name:'Item2',
+      url:'#'
+    },
+    {
+      name:'Item3',
+      url:'#'
+    },
+    {
+      name:'Item4',
+      url:'#'
+    },
+    {
+      name:'Item5',
+      url:'#'
+    }
+  ];
+
+  // this.setState({listItem:'qwe'});
 
 
-    var menuContent, logoBox, itemBox, sumTwoBox, listItem, listItemMain = [], listItemMobile = [];
-
-    itemBox =     document.getElementsByClassName('itemBox');
-    logoBox =     document.getElementsByClassName('logoBox');
-    menuContent = document.getElementsByClassName('menuContent');
-    sumTwoBox =   itemBox[0].clientWidth + logoBox[0].clientWidth;
-    listItem = [
-      {
-        name:'Item1',
-        url:'#'
-      },
-      {
-        name:'Item2',
-        url:'#'
-      },
-      {
-        name:'Item3',
-        url:'#'
-      },
-      {
-        name:'Item4',
-        url:'#'
-      },
-      {
-        name:'Item5',
-        url:'#'
-      }
-    ];
-
-    // this.setState({ listItemMain:listItem });
-    // this.setState({ listItemMobile:listItem });
-
-
-
-    console.log('Контент');
-    console.log(menuContent[0].clientWidth);
-    console.log('Сумма двух блоков');
-    console.log(sumTwoBox);
-
-    for (let i = 0; i < listItem.length; i++) {
-
-      if(menuContent[0].clientWidth > sumTwoBox){
-
-      console.log('true');
-        // listItemMain[i] = listItem[i];
-        // this.setState({ listItemMain:listItemMain });
-
-      }else{
-
-        console.log('false');
-        // listItemMobile[i] = listItem[i];
-        // this.setState({ listItemMobile:listItemMain });
-
-            }
-
-          }
-
-// console.log(listItemMain);
-// console.log('----------');
-// console.log(listItemMobile);
-// console.log('----------');
-
-// this.setState({ listItemMain:listItemMain, listItemMobile:listItemMobile });
-
-                  }
-
-// console.log(listItem);
-// listItem.splice(listItem.length - 1, 1);
-// console.log(listItem);
-
-
-resize();
-window.addEventListener('resize', ()=>{resize()});
+  // var resize = () =>{}
+  // resize();
+  // window.addEventListener('resize', ()=>{resize()});
 
 }
 
 
 
+
 render(){
 
-
-  // console.log(this.state.listItemMain);
-  // console.log('Мобильное '+this.listItemMobile);
-
-var item = this.state.listItemMain.map((item, i)=>
-  <li key={i} >{item.name}</li>
-);
+// var item = this.state.listItem.map((item, i)=>
+//   <li key={i} >{item.name}</li>
+// );
 
   return(
     <>
@@ -122,7 +78,7 @@ var item = this.state.listItemMain.map((item, i)=>
               <div className="itemBox">
 
                   <ul className="f-w-600 ul">
-                    {item}
+{this.state.listItem}
                   </ul>
 
                   <div className="mobileMenu"></div>
