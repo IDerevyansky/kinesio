7984e563f810d04e31b041e5555232e6da077698
“IDerevyansky” <IDerevyansky@yandex.ru>
2020-12-01 12:48:34 +0300

    -

--- front/kinesio-app/src/pages/main/Main.js
+++ front/kinesio-app/src/pages/main/Main.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './Main.css'
 
+import { dataPages } from '../pageStorage'
+
 class Main extends Component {
+
   render(){
+
+    const dataMain = dataPages.pages.main;
+    console.log(dataMain);
+
     return(
     <>
         <div className="bannerMainBox">
           <div className="imgBanner"></div>
+          <div className="textBannerContainer">
+            <div className="textMainBanner">
+              <h1>{dataMain.section.MainBanner.h1}</h1>
+              <p>{dataMain.section.MainBanner.p}</p>
+            </div>
+            <div className="callBackBtnMainBanner"></div>
+          </div>
         </div>
         <div className="bodyMainContent" >qwewqeqweqwewqwq</div>
     </>
