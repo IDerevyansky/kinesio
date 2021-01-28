35e84e70ab664c7a91817b1763abb45cf9bfdd27
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-01-28 03:15:43 +0300

    -

--- front/kinesio-app/src/pages/main/Main.js
+++ front/kinesio-app/src/pages/main/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css'
 
 // import CallBack from '../../components/callBack/callBack';
@@ -10,23 +11,30 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      dataMainBanner:''
+      dataMainBanner:'',
+      section_one_title:'',
+      section_one_body_text:'',
+      link_more_ru:''
     }
   }
 
   componentDidMount = async () => {
 
     // const respons = fetch(url, {method:"GET"}).then(res => res.json()).then(data => data.pages);
-    const respons = await fetch(url).then(pages => pages.json());
-    this.setState({dataMainBanner:respons.pages.main.section.MainBanner});
-    // console.log(respons.pages.main.section.MainBanner);
+    const respons = await fetch(url, { headers: {'Content-Type': 'multipart/form-data'} } ).then(pages => pages.json());
+    this.setState({
+      dataMainBanner:respons.pages.main.section.MainBanner,
+      section_one_title:respons.pages.main.section.Body.section_one.section_one_title,
+      section_one_body_text:respons.pages.main.section.Body.section_one.section_one_body_text,
+      link_more_ru:respons.pages.main.section.options.link_more_ru
+    });
+    // console.log(respons.pages.main.section.MainBanner.h1);
   }
 
 
   render(){
 
-
-
+    
     return(
     <>
           <div className="contentBanner">
@@ -50,8 +58,31 @@ class Main extends Component {
 
           </div>
 
-        <div className="bodyMainContent" >qwewqeqweqwewqwq</div>
-  </>
+        <div className="bodyMainContent">
+
+          <div className="section-one">
+
+            <div className="section-one__content">
+
+                  <div className="section-one__title">
+
+                    <h2>{this.state.section_one_title}</h2> 
+
+                  </div>
+                  <div className="section-one__body-text">
+
+                    <p>{this.state.section_one_body_text}</p>
+                    
+                    <Link  to={'/whatIs'} rel="noopener noreferrer" ><p className="more">{this.state.link_more_ru}</p></Link>
+
+                  </div>
+
+            </div>
+
+          </div>
+
+        </div>
+    </>
     )
   }
 }
