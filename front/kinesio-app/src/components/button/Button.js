10b98191a66880ca269ccbf2479d164ea1f74bcb
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-03-26 01:36:56 +0300

    -

--- front/kinesio-app/src/components/button/Button.js
+++ front/kinesio-app/src/components/button/Button.js
@@ -6,11 +6,21 @@ import "./Button.css";
 
 class Button extends Component  {
     render(){
+
+        let click;
+
+        if(typeof this.props.statusPopUp === 'function'){
+            click = ()=>{this.props.statusPopUp(true)};
+        }else{
+            click = ()=>{};
+        }
+
         return(
-            <Link to={this.props.link} rel="noopener noreferrer" >
-                <div onClick={()=>{this.props.statusPopUp(true)}} className="btn">{this.props.text.toUpperCase()}</div>
+            <Link to={this.props.link?this.props.link:''} rel="noopener noreferrer" >
+                <div onClick={click} className="btn">{this.props.text.toUpperCase()}</div>
             </Link>
         )
+
     }
 }
 
