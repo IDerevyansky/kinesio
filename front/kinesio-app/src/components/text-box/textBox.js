9b0074265370e8168ef49dfc4b7835bc90333109
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-03-26 14:15:09 +0300

    -

--- front/kinesio-app/src/components/text-box/textBox.js
+++ front/kinesio-app/src/components/text-box/textBox.js
@@ -13,24 +13,14 @@ class TextBox extends Component {
             <div className="container m-auto container-text-box">
                 <div className={ this.props.orientation == 'y'?'content-text-box-column':'content-text-box-row' }>
                     <div className={ this.props.orientation == 'y'?'title-text-box-column':'title-text-box-row' }>
-                        <h2>Что нужно знать о кинезиологии</h2>
+                        <h2 dangerouslySetInnerHTML={{__html:this.props.title}}></h2>
                     </div>
                     <div className= { this.props.orientation == 'y'?'textBody-text-box-column':'textBody-text-box-row' } >
-                        <p>
-                        Он вкалывал все эти десять лет не для того, чтобы теперь замедлить ход к уже близкой победе. 
-                        Я хочу поздравить вас всех с успешным завершением нашего дела – проект «Хинни и Смит» закончился 
-                        очень удачно для всех нас. Теперь мы можем сами доставлять товары во все уголки страны, сокращая 
-                        расходы и увеличивая прибыль. – Рик поднял бокал с шампанским. – За замечательную команду с блестящим 
-                        будущим! Он сделал глоток из бокала под гул всеобщего одобрения.<br/><br/>
 
-                        Его взгляд снова скользнул к отчужденной, но удивительно милой незнакомке. Девушка все так же стояла 
-                        в дверях, наблюдая за его сотрудниками. У нее в руках не было бокала. И это нужно исправить. Мужчина 
-                        опять потер подбородок. Следующей задачей было слияние с компанией «Спорта Ко» и удвоение объема продаж 
-                        их спортивного оборудования. Разумеется, было весьма рискованно предпринимать этот шаг так быстро, 
-                        но Рик не мог ждать.
-                        </p>
+                        <p dangerouslySetInnerHTML={{__html:this.props.text}} ></p>
+
                             <div className="m24-t">
-                                <Link to="" className="more"><p>Подробнее</p></Link>
+                                <Link to={this.props.link?this.props.link:''} className="more"><p>Подробнее</p></Link>
                             </div>
                     </div>
                 </div>    
@@ -41,3 +31,5 @@ class TextBox extends Component {
 }
 
 export default TextBox;
+
+
